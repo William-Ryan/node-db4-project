94f3e7dec7bc03ce038099dd3e537f82b52cb2e5
William <billy.ryan.rom@gmail.com>
2020-03-23 00:51:28 -0400

    Add GET requests for recipe ingredients and steps

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -16,11 +16,11 @@ function getShoppingList(id){
     .select('r.recipe_name as recipe', 'i.ingredient', 'i.measurement')
     .join('recipe_ingredients as ri', 'r.id', '=', 'ri.recipe_id')
     .join('ingredients as i', 'i.id', '=', 'ri.ingredient_id')
-    .where(id)
+    .where('r.id', '=', id)
 }
 
 function getInstructions(id){
     return db('recipes as r')
     .select('r.recipe_name as recipe', 'r.steps')
-    .where(id)
+    .where('r.id', '=', id)
 }
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -15,4 +15,36 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id/ingredients', (req, res) => {
+    const { id } = req.params;
+
+    Recipe.getShoppingList(id)
+    .then(list => {
+        if(list.length){
+            res.status(200).json(list)
+        } else {
+            res.status(404).json({ message: 'Could not find this recipes ingredients' })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Error Retrieving this Recipe'})
+    })
+})
+
+router.get('/:id/steps', (req, res) => {
+    const { id } = req.params;
+
+    Recipe.getInstructions(id)
+    .then(steps => {
+        if(steps.length){
+            res.status(200).json(steps)
+        } else {
+            res.status(404).json({ message: 'Could not find this recipes steps' })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Error Retrieving this Recipe' })
+    })
+})
+
 module.exports = router
